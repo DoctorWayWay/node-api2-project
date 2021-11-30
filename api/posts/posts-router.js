02bfc6567e166d8fc0336052b4539e4daf9f478a
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-11-30 15:21:36 -0800

    added endpoint that finds comments of post by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -104,5 +104,25 @@ router.delete("/:id", async (req, res) => {
   }
 })
 
+// [GET] /api/posts/:id/comments (finds and returns a post's comments)
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const { id } = req.params
+    const post = await Posts.findById(id)
+    if (!post) {
+      res.status(404).json({
+        "message": "The post with the specified ID does not exist"
+      })
+    } else {
+      const postComments = await Posts.findPostComments(id)
+      res.status(200).json(postComments)
+    }
+  } catch (err) {
+    res.status(500).json({
+      "message": "The comments information could not be retrieved"
+    })
+  }
+})
+
 // ===== POST-ROUTER EXPORT =====
 module.exports = router
