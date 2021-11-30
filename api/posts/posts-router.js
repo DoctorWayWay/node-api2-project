0cdca96ca981ca793983c21dc124a2f28793b08d
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-11-30 13:17:27 -0800

    added express, started work on the server files

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,14 @@
 // implement your posts router here
+
+// ===== IMPORTS =====
+const express = require("express")
+// const Posts = require("./posts-model")
+
+// ===== INSTANCE OF EXPRESS =====
+const router = express.Router()
+
+// ===== ENDPOINTS =====
+
+
+// ===== POST-ROUTER EXPORT =====
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,2 +1,21 @@
 // implement your server here
 // require your posts router and connect it here
+
+// ===== IMPORTS =====
+const express = require("express")
+const postsRouter = require("./posts/posts-router")
+
+// ===== INSTANCE OF EXPRESS =====
+const server = express()
+
+// ===== GLOBAL MIDDLEWARE =====
+server.use(express.json())
+server.use("/api/posts", postsRouter)
+
+// ===== ENDPOINTS =====
+server.get("/", (req, res) => {
+  res.status(200).send(`<h2>Welcome to my Blog Post API Module Project</h2>`)
+})
+
+// ===== SERVER EXPORT =====
+module.exports = server
