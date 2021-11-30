6ea5816fb4410d53fc95d1568d224f8ad759fded
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-11-30 13:34:47 -0800

    added get endpoint that sends all posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -2,13 +2,23 @@
 
 // ===== IMPORTS =====
 const express = require("express")
-// const Posts = require("./posts-model")
+const Posts = require("./posts-model")
 
 // ===== INSTANCE OF EXPRESS =====
 const router = express.Router()
 
 // ===== ENDPOINTS =====
-
+// [GET] /api/posts (returns all posts)
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Posts.find()
+    res.status(200).json(posts)
+  } catch (err) {
+    res.status(500).json({
+      "message": "The posts information could not be retrieved"
+    })
+  }
+})
 
 // ===== POST-ROUTER EXPORT =====
 module.exports = router
